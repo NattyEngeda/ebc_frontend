260cc685beeb302e4901018552fd76e83b2917ef
Naatty123 <nattynengeda@gmail.com>
2022-12-25 21:40:48 +0300

    Edit Commit

--- pages/cards/components/Message.js
+++ pages/cards/components/Message.js
@@ -1,17 +1,28 @@
+import { Accordion, TextInput, Textarea } from '@mantine/core';
+
 const Message = () => {
     return (
-        <>
-            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
-                <div className="collapse-title text-xl font-medium">
-                    Send A Message !!
-                </div>
-                <div className="collapse-content">
-                    <div className="w-full flex flex-col gap-5">
-                        <input type="text" placeholder="Name" className="input input-bordered w-full max-w-xs" />
-                    </div>
-                </div>
-            </div>
-        </>
+        <div className='boorshad'>
+            <Accordion defaultValue="contact">
+                <Accordion.Item value="contact">
+                    <Accordion.Control>Contact Me !!</Accordion.Control>
+                    <Accordion.Panel>
+                        <div className='w-full flex flex-col gap-5'>
+                            <TextInput
+                                label="Name"
+                            />
+                            <TextInput
+                                label="Email / Phone"
+                            />
+                            <Textarea
+                                label="Message"
+                            />
+                            <button className='py-3 bg-blue-800 text-white rounded-md'>Send</button>
+                        </div>
+                    </Accordion.Panel>
+                </Accordion.Item>
+            </Accordion>
+        </div>
     );
 }
 

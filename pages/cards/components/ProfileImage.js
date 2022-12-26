2a37a0e28f9e4b84fc589c3e28d3f6b512a439a7
Naatty123 <nattynengeda@gmail.com>
2022-12-26 22:14:01 +0300

    LIttle Edit Commit

--- pages/cards/components/ProfileImage.js
+++ pages/cards/components/ProfileImage.js
@@ -6,7 +6,7 @@ const ProfileImage = () => {
                     className="w-auto h-[35vh] object-fill bg-center"
                     src="https://images2.content-hci.com/commimg/myhotcourses/blog/post/myhc_89683.jpg" alt="Background Image" />
             </div>
-            <div className="absolute bottom-0 avatar">
+            <div className="absolute bottom-0 avatar w-48">
                 <div className="w-full rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                     <img src="https://media.licdn.com/dms/image/D4E03AQF-pGhCwv_e0g/profile-displayphoto-shrink_200_200/0/1664565500017?e=1675900800&v=beta&t=trGa7kI9h92yvK7ANYOhuwZScokGUXHBrcs3DGfvpOs" />
                 </div>
--- pages/cards/components/Social.js
+++ pages/cards/components/Social.js
@@ -3,16 +3,16 @@ import { BsFacebook, BsTelegram, BsInstagram, BsWhatsapp, BsGlobe } from 'react-
 const Social = () => {
     return (
         <div className="flex flex-row px-5 gap-3 py-10">
-            <div className="socialiconcontainer bg-blue-700">
+            <div className="socialiconcontainer bg-blue-700 p-3">
                 <BsFacebook className="socialicons " />
             </div>
-            <div className="socialiconcontainer bg-blue-800">
+            <div className="socialiconcontainer bg-blue-800 p-3">
                 <BsTelegram className="socialicons" />
             </div>
-            <div className="socialiconcontainer bg-pink-700">
+            <div className="socialiconcontainer bg-pink-700 p-3">
                 <BsInstagram className="socialicons" />
             </div>
-            <div className="socialiconcontainer bg-green-500">
+            <div className="socialiconcontainer bg-green-500 p-3">
                 <BsWhatsapp className="socialicons" />
             </div>
         </div>

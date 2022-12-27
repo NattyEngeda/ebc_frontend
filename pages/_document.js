ac1b069b9209666f9824fffd219e770478cc466c
NattyEngeda <nattynengeda@gmail.com>
2022-12-27 20:49:34 +0300

    Commit

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" data-theme="light">
-      <Head />
+      <Head>
+         <link rel="manifest" href="../manifest.json" />
+         <meta name="theme-color" content='#fff'/>
+      </Head>
       <body>
         <Main />
         <NextScript />

fb7c945d9b430c11bc82f53d7711081323d5c427
NattyEngeda <nattynengeda@gmail.com>
2023-01-01 21:47:48 +0300

    Change Commit

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,11 +1,11 @@
-// import React, { useState } from 'react';
-// import Header from '../components/header';
-// import { TextInput, PasswordInput } from '@mantine/core';
-// import { useForm } from '@mantine/form';
-// import axios from '../components/axios';
+import React, { useState } from 'react';
+import Header from '../components/header';
+import { TextInput, PasswordInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import axios from '../components/axios';
 
 // // Notifications
-// import Alert from '../components/alert';
+import Alert from '../components/alert';
 // Notifications
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -29,46 +29,46 @@ const Index = () => {
     };
 
 
-//     const loginForm = useForm({
-//         initialValues: {
-//             username: '',
-//             password: '',
-//         },
-//         validate: {
-//             username: (value) => (value.length < 2 ? 'username must be moret han 2' : null),
-//             password: (value) => (value.length < 4 ? 'password must be more than 8' : null)
-//         }
-//     });
+    const loginForm = useForm({
+        initialValues: {
+            username: '',
+            password: '',
+        },
+        validate: {
+            username: (value) => (value.length < 2 ? 'username must be moret han 2' : null),
+            password: (value) => (value.length < 4 ? 'password must be more than 8' : null)
+        }
+    });
 
-//     const loginSubmit = (values) => {
-//         axios.post('/login', values, {
-//             withCredentials: true,
-//         }).then((response) => {
-//             console.log(response.data);
-//             notify('Hello')
-//         })
+    const loginSubmit = (values) => {
+        axios.post('/login', values, {
+            withCredentials: true,
+        }).then((response) => {
+            console.log(response.data);
+            notify('Hello')
+        })
 
-//     }
-//     const loginError = (errors) => {
-//         console.log(errors);
-//     }
+    }
+    const loginError = (errors) => {
+        console.log(errors);
+    }
 
-//     const signupForm = useForm({
-//         initialValues: {
-//             fname: '',
-//             lname: '',
-//             uname: '',
-//             email: '',
-//             password: '',
-//         },
-//         validate: {
-//             fname: (value) => (value.length < 2 ? 'Add more Letter' : null),
-//             lname: (value) => (value.length < 2 ? 'Add more Letter' : null),
-//             uname: (value) => (value.length < 2 ? 'Add more Letter' : null),
-//             email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-//             password: (value) => (value.length < 8 ? 'Passowrd Must be greater that 8 ' : null),
-//         }
-//     })
+    const signupForm = useForm({
+        initialValues: {
+            fname: '',
+            lname: '',
+            uname: '',
+            email: '',
+            password: '',
+        },
+        validate: {
+            fname: (value) => (value.length < 2 ? 'Add more Letter' : null),
+            lname: (value) => (value.length < 2 ? 'Add more Letter' : null),
+            uname: (value) => (value.length < 2 ? 'Add more Letter' : null),
+            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+            password: (value) => (value.length < 8 ? 'Passowrd Must be greater that 8 ' : null),
+        }
+    })
 
     const signupSubmit = (values) => {
         axios.post('/signup', values, {
@@ -86,32 +86,32 @@ const Index = () => {
             notify('Error', 'error');
         })
 
-//     }
-//     const signupError = (errors) => {
-//         console.log(errors);
-//     }
+    }
+    const signupError = (errors) => {
+        console.log(errors);
+    }
 
 
-//     return (
-//         <>
-//             <Header />
-//             <main className="w-full py-28 flex items-cener justify-center px-5">
-//                 <div className="w-full md:w-1/3 rounded-xl shadow-xl h-auto boorshad shadow-gray-400 py-10">
-//                     <div
-//                         className="text-center flex flex-col items-center gap-2 cursor-pointer"
-//                         onClick={() => {
-//                             setAuthState('start');
+    return (
+        <>
+            <Header />
+            <main className="w-full py-28 flex items-cener justify-center px-5">
+                <div className="w-full md:w-1/3 rounded-xl shadow-xl h-auto boorshad shadow-gray-400 py-10">
+                    <div
+                        className="text-center flex flex-col items-center gap-2 cursor-pointer"
+                        onClick={() => {
+                            setAuthState('start');
 
-//                         }}
-//                     >
-//                         <h1 className="text-3xl font-Sanchez">Login or Signup</h1>
-//                         <hr className="w-1/3 text-gray-400 border border-gray-400" />
-//                     </div>
-//                     {authState == "start" &&
-//                         <div className="flex flex-col gap-5 px-5 md:px-10 py-5">
-//                             <button
-//                                 onClick={() => {
-//                                     setAuthState('login');
+                        }}
+                    >
+                        <h1 className="text-3xl font-Sanchez">Login or Signup</h1>
+                        <hr className="w-1/3 text-gray-400 border border-gray-400" />
+                    </div>
+                    {authState == "start" &&
+                        <div className="flex flex-col gap-5 px-5 md:px-10 py-5">
+                            <button
+                                onClick={() => {
+                                    setAuthState('login');
 
                                 }}
                                 className='btn'>Login</button>
@@ -154,16 +154,16 @@ const Index = () => {
                                     label="Last Name"
                                     {...signupForm.getInputProps('lname')}
 
-//                                 />
-//                             </div>
-//                             <TextInput
-//                                 label="Username"
-//                                 {...signupForm.getInputProps('uname')}
+                                />
+                            </div>
+                            <TextInput
+                                label="Username"
+                                {...signupForm.getInputProps('uname')}
 
-//                             />
-//                             <TextInput
-//                                 label="Email"
-//                                 {...signupForm.getInputProps('email')}
+                            />
+                            <TextInput
+                                label="Email"
+                                {...signupForm.getInputProps('email')}
 
                             />
                             <PasswordInput

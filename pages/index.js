790c9ad4b9dc4a48419afeb740c01bb59019f866
NattyEngeda <nattynengeda@gmail.com>
2023-01-22 18:39:55 +0300

    SEO edit Commit

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
     <>
       <Head>
         <title>Yene Card</title>
+        {/* <meta name="robots" content="noindex,nofollow" /> */}
         <meta name="description" content="Geenerated By Alamondai" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />

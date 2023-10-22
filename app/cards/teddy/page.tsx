3fde57b8b8b6d2fcb48af27093ea8e570c147901
NattyEngeda <nattynengeda@gmail.com>
2023-10-22 18:16:20 +0300

    Commit Todays Last

--- app/cards/teddy/page.tsx
+++ app/cards/teddy/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Teddy() {
   return (
-    <main>
-
+    <main className='flex flex-col w-full bg-gradient-to-br  from-[#3E97D1] via-violet-500 to-[#3F6D8C] min-h-screen  mx-auto'>
+      <p>Hello</p>
     </main>
   )
 }
